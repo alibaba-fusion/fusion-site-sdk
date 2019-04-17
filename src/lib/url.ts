507b159745fc58352bbb6498ca26f79b7ba9dcb8
暮尘 <i@muchen.me>
2019-04-17 14:06:56 +0800

    feat: add url

---
+++ src/lib/url.ts
@@ -0,0 +1,19 @@
+import {Env} from '../type';
+
+const URLS = {
+    [Env.prod]: 'aHR0cHM6Ly9mdXNpb24uZGVzaWdu',
+    [Env.pre]: 'aHR0cHM6Ly9wcmUtd3d3LmZ1c2lvbi5kZXNpZ24=',
+    [Env.daily]: 'aHR0cHM6Ly9mdXNpb24udGFvYmFvLm5ldA==',
+    [Env.local]: 'aHR0cDovLzEyNy4wLjAuMTo3MDAx',
+    [Env.alidaily]: 'aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS5uZXQ',
+    [Env.alipre]: 'aHR0cHM6Ly9wcmUtZnVzaW9uLmFsaWJhYmEtaW5jLmNvbQ==',
+    [Env.aliprod]: 'aHR0cHM6Ly9mdXNpb24uYWxpYmFiYS1pbmMuY29t',
+};
+
+export default function getUrl(env?: Env): string {
+    if (!env) {
+        env = Env.prod;
+    }
+    const str = URLS[env];
+    return Buffer.from(str, 'base64').toString('utf-8');
+}
